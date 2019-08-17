5dff5cfb28c2b57ea76378a18e09f238ebab6b6f
RocMark <onigir77799@gmail.com>
2019-08-17 11:06:06 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -11,16 +11,24 @@ const userRequest = axios.create({
 
 // 關閉 Prettier & phpfmt 
 export default new Vuex.Store({
+  // getters 等同於 state 的 computed 可以先做運算
+  // mapGetter/mapState 用以簡化呼叫多個變數的過程
+  
+  // 將 this.varA 映射為 store.state.varA
+  // computed: mapState(['varA,varB'])
+  getters: {
+    // doneTodos: state => { return state.todos.filter(todo => todo.done) }
+  }
   state: {
-
-  },
-  mutations: {
 
   },
   actions: {
     getUsers() {
       return userRequest.get('/users')
     },
+  },
+  mutations: {
+
   },
   strict: process.env.NODE_ENV !== 'production'
 })
